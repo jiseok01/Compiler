3e9eaf39b500c917154ff25cf713331b01e02cc4
jiseok01 <126743121+jiseok01@users.noreply.github.com>
2024-11-18 01:48:21 +0900

    Add files via upload

---
+++ ParserTree/node.h
@@ -0,0 +1,11 @@
+typedef struct _node {
+	char 	kind[10];
+	union {
+		double dv;
+		char * sv;
+	}		value;
+	struct _node * 	bro;
+	struct _node * 	son;
+} Node;
+
+Node * makeNode(char *, Node *, Node *);
